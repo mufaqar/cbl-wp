ce54d54b48a58d9b196923933aca024f5ec6dbb2
mufaqar <mufaqar@gmail.com>
2023-08-07 20:27:28 +0500

    updated

--- temp-result.php
+++ temp-result.php
@@ -6,13 +6,14 @@ get_header();
 
 $args = array(
     'post_type' => 'providers',    
-    // 'meta_query' => array(
-    //     array(
-    //         'key' => 'internet_serices',
-    //         'value' => array('01005'),
-    //         'compare' => 'IN', 
-    //    ),
-    // ),
+    'meta_query' => array(
+        array(
+            'key' => 'internet_serices',
+           // 'value' => array('15401'),
+            'value' => '15401',
+            'compare' => 'EXISTS',
+       ),
+    ),
 );
 
 $query = new WP_Query($args);
@@ -21,7 +22,7 @@ if ($query->have_posts()) {
     while ($query->have_posts()) {
         $query->the_post();
         // Your post content display code here
-        the_title();
+        the_title( '<h3>', '</h3>' ); 
  
     }
     wp_reset_postdata(); // Reset post data to the main query
