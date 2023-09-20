f7748f83a98a74402e17531bfabdd17d58dbdd32
mufaqar <mufaqar@gmail.com>
2023-09-20 12:58:51 +0500

    CPTS

--- functions.php
+++ functions.php
@@ -256,6 +256,46 @@ function cptui_register_my_taxes_providers_types() {
 }
 add_action( 'init', 'cptui_register_my_taxes_providers_types' );
 
+
+function cptui_register_my_taxes_providers_service_types() {
+
+	/**
+	 * Taxonomy: Services Type.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Services", "custom-post-type-ui" ),
+		"singular_name" => esc_html__( "Service", "custom-post-type-ui" ),
+	];
+
+	
+	$args = [
+		"label" => esc_html__( "Services", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => true,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'providers_service_types', 'with_front' => true,  'hierarchical' => true, ],
+		"show_admin_column" => true,
+		"show_in_rest" => true,
+		"show_tagcloud" => false,
+		"rest_base" => "providers_service_types",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"rest_namespace" => "wp/v2",
+		"show_in_quick_edit" => true,
+		"sort" => true,
+		"show_in_graphql" => true,
+		"graphql_single_name" => "ServiceType",
+		"graphql_plural_name" => "ServiceTypes",
+	];
+	register_taxonomy( "providers_service_types", [ "providers" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_providers_service_types' );
+
 add_action( 'init', 'cptui_register_my_cpts' );
 
 function cptui_register_my_cpts_area_zone() {
