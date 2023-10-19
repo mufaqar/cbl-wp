4a6b9866ea5a53803a257b313efc88ce6f7d7e92
mufaqar <mufaqar@gmail.com>
2023-10-19 17:08:09 +0500

    CF

--- home.php
+++ home.php
@@ -13,8 +13,23 @@
         ?>
         <h2> <?php the_title()?></h2>
 
-        <h2> <?php  echo $price?></h2>
+        <p> <?php  
+
+        // Get all custom fields for the current post
+        $custom_fields = get_post_custom();
+
+        // Loop through and display the custom fields
+        foreach ($custom_fields as $key => $value) {
+            echo '<p><strong>' . $key . ':</strong> ' . implode(', ', $value) . '</p>';
+        }
+
+        // You can also get specific custom fields by key if needed
+        $specific_field = get_post_meta(get_the_ID(), 'your_custom_field_key', true);
+        if ($specific_field) {
+            echo '<p><strong>Your Specific Custom Field:</strong> ' . $specific_field . '</p>';
+        }
 	
+      ?></p>
 	
 		  <?php endwhile; wp_reset_query(); else : ?>
 			<h2><?php _e('Nothing Found','lbt_translate'); ?></h2>
